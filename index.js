931d96df028ba47222ab0d9a41938d561811861b
RiaJul Pro <riajulpro@outlook.com>
2023-11-30 20:15:35 +0600

    Adding the server side origin into the cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://exforum24.web.app", "http://localhost:5173"],
     credentials: true,
   })
 );
